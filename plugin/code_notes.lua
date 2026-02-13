63da6ccf58d39ddedbad5ec34b0dfa043fa06970
nisidabay <nisidabay@gmail.com>
2026-02-13 20:53:21 +0100

    Modify plugin/code_notes.lua

--- plugin/code_notes.lua
+++ plugin/code_notes.lua
@@ -1,7 +1,21 @@
 -- plugin/code_notes.lua
--- Auto-loads the module. No setup needed.
+-- Auto-loads the module and creates user commands
 
--- Optional: add a command for convenience
+local notes = require('code_notes')
+
+-- User commands for convenience
 vim.api.nvim_create_user_command('CodeNotesList', function()
-    require('code_notes').list_notes()
+    notes.list_notes()
 end, { desc = 'List all code notes' })
+
+vim.api.nvim_create_user_command('CodeNotesDelete', function()
+    notes.delete_notes()
+end, { desc = 'Delete code notes interactively' })
+
+vim.api.nvim_create_user_command('CodeNotesClear', function()
+    notes.clear_all_notes()
+end, { desc = 'Clear all code notes' })
+
+vim.api.nvim_create_user_command('CodeNotesAdd', function()
+    notes.add_note()
+end, { desc = 'Add a new code note' })
